f6f0cb20c46e27f4405686405d1cfcb50a09a397
samim ansari <samimansari02@gmail.com>
2023-10-31 19:22:35 +0530

    logic of square board

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,8 +1,8 @@
 
 
-const Square = ({value}) => {
+const Square = ({value ,onClick}) => {
   return (
-    <button className="square" type="button">{value}</button>
+    <button className="square" type="button" onClick={onClick}>{value}</button>
   )
 }
 
